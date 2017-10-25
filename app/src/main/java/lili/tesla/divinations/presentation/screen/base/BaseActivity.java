ae451294ae5ab5d396f0ce3252f0eacb2e32b795
Лилия Габбасова <liliangelok@mail.ru>
2017-10-25 21:29:31 +0500

    create base activity & presenter

---
+++ app/src/main/java/lili/tesla/divinations/presentation/screen/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package lili.tesla.divinations.presentation.screen.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Лилия on 25.10.2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
